355458c80a7313f8bc05703ad1c661d4652e7545
Thomas Cheng <contact.chengthomas@gmail.com>
2018-08-13 13:04:57 -0400

    Added decoding functionality.

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,37 @@ let key;
 let message;
 let keyArray = [];
 
-function main(){
-    onClickFunc();
-    generateKeyList();
-    document.getElementById("output-message").innerHTML = encodeMessage();
+function mainEncode(){
+    if (onClickFunc()){
+        generateKeyList();
+        document.getElementById("output-message").innerHTML = encodeMessage();
+    }
+}
+
+function mainDecode(){
+    if (onClickFunc()){
+        generateKeyList();
+        document.getElementById("output-message").innerHTML = decodeMessage();
+    }
+}
+
+function decodeMessage(){
+    var keyIndex = 0;
+    var returnMessage = [];
+    for (i = 0; i < message.length; i++){
+        if (keyIndex > key.length-1){
+            keyIndex = 0;
+        }
+        var letterIndex = keyArray[keyIndex].indexOf(message.charAt(i));
+        if (letterIndex === -1){
+            returnMessage.push(' ');
+        }
+        else{
+            returnMessage.push(String.fromCharCode(65+letterIndex));
+            keyIndex++;
+        }
+    }
+    return (returnMessage.join(''));
 }
 
 function onClickFunc(){
@@ -15,8 +42,9 @@ function onClickFunc(){
 
     if (key == "" || message == ""){
         alert("Please enter String values in the text fields.");
+        return (false);
     }
-    document.getElementById("output-message").innerHTML = key + " " + message;
+    return (true);
 }
 
 function encodeMessage(){
@@ -29,6 +57,7 @@ function encodeMessage(){
         var currentColumn = message.charCodeAt(i) - 65;
         // checks to see if the letter is indeed a valid character
         if (currentColumn < 0 || currentColumn > 25){
+            returnMessage.push(" ");
             continue;
         }
         returnMessage.push(keyArray[keyIndex][currentColumn]);
